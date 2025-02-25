759583a99d8a91a5e713e76705ec24412d989240
serhiichyipesh <chyipeshserhii@gmail.com>
2025-02-26 00:01:45 +0200

    removed globals from eslint config again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 import pluginJs from '@eslint/js';
 import pluginQuery from '@tanstack/eslint-plugin-query';
 import pluginReact from 'eslint-plugin-react';
+import globals from './globals';
 import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
